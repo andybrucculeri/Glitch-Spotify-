1a8203b0ae224f6d652885f6a2dfbd6a4bbbaaee
Glitch (andybrucculeri-glitch-spotify-) <none>
2019-02-12 17:55:16 +0000

    🐓😤 Checkpoint ./public/client.js:3109124/3837 ./server.js:3109124/56

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/category-playlists', function (request, response) {
   
   // Get playlists from a browse category
   // Find out which categories are available here: https://beta.developer.spotify.com/console/get-browse-categories/
-  spotifyApi.getPlaylistsForCategory('jazz', { limit : 5 })
+  spotifyApi.getPlaylistsForCategory('pop', 'DE', { limit : 10 })
     .then(function(data) {
     
     // Send the list of playlists
@@ -106,7 +106,7 @@ app.get('/artist', function (request, response) {
 app.get('/artist-top-tracks', function (request, response) {
   
   // Get an artist's top tracks in a country
-  spotifyApi.getArtistTopTracks('0LcJLqbBmaGUft1e9Mm8HV', 'SE')
+  spotifyApi.getArtistTopTracks('0LcJLqbBmaGUft1e9Mm8HV', 'US')
     .then(function(data) {
     
       // Send the list of tracks
